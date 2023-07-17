fab325356ec0c82c76c966b137d5687271ec2837
an <anthedev@gmail.com>
2023-07-17 10:27:08 -0700

    workshop 4 assignment corrections

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,10 @@ $(function() {
 
     $("#reserveButton").click(function() {
         $("#reserveModal").modal("show");
-    })
+    });
 
     $("#loginButton").click(function() {
         $("#loginModal").modal("show");
-    })
+    });
 
 });
